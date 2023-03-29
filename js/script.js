f6800b5dac774460ce670f0148a079035cc29895
Marcelo Aparecido Malvezi Junior <62224726+MarceloJr006@users.noreply.github.com>
2023-03-29 11:52:57 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+function mostra()
+{
+    let x = document.getElementById("imagem");
+    let y = document.getElementById("botao1")
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+        y.innerHTML = "Ocultar";
+    }
+    else
+    {
+        x.style.display = "none";
+        y.innerHTML = "Mostrar";
+    }
+}
+
+function mostra2()
+{
+    let x = document.getElementById("imagem2");
+    let y = document.getElementById("botao2")
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+        y.innerHTML = "Ocultar";
+    }
+    else
+    {
+        x.style.display = "none";
+        y.innerHTML = "Mostrar";
+    }
+}
+
+function mostra3()
+{
+    let x = document.getElementById("imagem3");
+    let y = document.getElementById("botao3")
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+        y.innerHTML = "Ocultar";
+    }
+    else
+    {
+        x.style.display = "none";
+        y.innerHTML = "Mostrar";
+    }
+}
+
+function mostra4()
+{
+    let x = document.getElementById("imagem4");
+    let y = document.getElementById("botao4")
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+        y.innerHTML = "Ocultar";
+    }
+    else
+    {
+        x.style.display = "none";
+        y.innerHTML = "Mostrar";
+    }
+}
